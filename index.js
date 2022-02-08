6ebaad2f97b0feb5cb5f09645beae5bc8eaebb0d
Bex Akeakamai <bex.akeakamai@gmail.com>
2022-02-08 11:58:14 -0500

    add event listener

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const btn = document.getElementById("btn");
+const password1 = document.getElementById("p1");
+const password2 = document.getElementById("p2");
+const password3 = document.getElementById("p3");
+const password4 = document.getElementById("p4");
+
 // use map function to create alphabet array
 
 const alpha = Array.from(Array(26)).map((e, i) => i + 65);
@@ -45,19 +51,20 @@ const lowerCode = 1;
 const specialCode = 2;
 const numbCode = 3;
 
-const getRandomInt = (max) => Math.floor(Math.random() * max);
-let randomUpper = upperCase[getRandomInt(upperCase.length)];
-let randomLower = upperCase[getRandomInt(upperCase.length)].toLowerCase();
-let randomSpecial = specialChars[getRandomInt(specialChars.length)];
-let randomNum = numbs[getRandomInt(numbs.length)];
-
-console.log(randomUpper);
-console.log(randomLower);
-console.log(randomSpecial);
-console.log(randomNum);
-
 let password = "";
 
 // for (let i = 0; i < passwordLength; i++) {
 //     getRandomInt(4) === upperCode ? password +=
 // }
+btn.addEventListener("click", function () {
+  const getRandomInt = (max) => Math.floor(Math.random() * max);
+  let randomUpper = upperCase[getRandomInt(upperCase.length)];
+  let randomLower = upperCase[getRandomInt(upperCase.length)].toLowerCase();
+  let randomSpecial = specialChars[getRandomInt(specialChars.length)];
+  let randomNum = numbs[getRandomInt(numbs.length)];
+
+  console.log(randomUpper);
+  console.log(randomLower);
+  console.log(randomSpecial);
+  console.log(randomNum);
+});
